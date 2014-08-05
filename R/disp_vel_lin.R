eb8dbbe6fe40493169441b1b25a69cb83347c544
aousabdo <aousabdo@gmail.com>
2014-08-05 16:32:34 -0400

    Added a comment

--- R/disp_vel_lin.R
+++ R/disp_vel_lin.R
@@ -1,3 +1,4 @@
+## A function to ...
 disp_vel_lin <-  function(Vx0=10, t=10, b=0.1, m=0.1){
         #' Function to calculate displaement and velocity of a projectile when air friction is introduced
         X <- Vx0*m/b*(1-exp(-t*b/m))
