b869d0f59c9be97c5a8891295e2ce8562a537619
aousabdo <aousabdo@gmail.com>
2014-08-08 00:22:01 -0400

    Edited examples

--- R/projFrictionLin.R
+++ R/projFrictionLin.R
@@ -16,25 +16,20 @@
 #' @return y Displacement in the vertical direction as a function of time (in meters)
 #' @return vy speed in the vertical direction as a function of time (in m/s units)
 #' @return y_x Displacement in the vertical direction as a function of horizontal displacement (in meters)
+#' 
+#' @seealso \code{\link{Projectile}}
+#' 
 #' @examples
 #' 
 #' projFrictoinLin(t = seq(1,5,0.1), y0 = 10, v0 = 50, theta0 = 30, b = 0.5, m = 5)
 #' 
 #' ## Example of how to call both projectile functions
 #' ## and make a projectile path plot 
-#' t      <- seq(0,5,0.1)
-#' v0     <- 50 
-#' theta0 <- 30
-#' b      <- 0.9
-#' m      <- 5
-#'  
-#' vacuum   <- Projectile(t, y0 = 10, v0 = v0, theta0 = theta0)
-#' friction <- projFrictoinLin(t, y0 = 10, v0 = v0, theta0 = theta0, b = b, m = m)
 #' 
-#' x  <- vacuum$x
-#' y  <- vacuum$y
-#' xf <- friction$x
-#' yf <- friction$y_x
+#' vacuum   <- Projectile(t = seq(0,5,0.1), y0 = 10, v0 = 50, theta0 = 30) 
+#' friction <- projFrictoinLin(t = seq(0,5,0.1), y0 = 10, v0 = 50, theta0 = 30, b = 0.5, m = 5) 
+#' 
+#' x  <- vacuum$x; y  <- vacuum$y; xf <- friction$x; yf <- friction$y_x
 #' 
 #' plot(x, y, col="blue", pch=19, ylim=c(min(y, yf), max(y, yf)), 
 #'      xlab = "Horizontal Distance Travelled (m)", 
