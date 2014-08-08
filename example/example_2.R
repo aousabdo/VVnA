d39cf908c6df3b21ffc38ee8f9cd33784dce16e0
aousabdo <aousabdo@gmail.com>
2014-08-08 16:11:29 -0400

    Modified function

--- example/example_2.R
+++ example/example_2.R
@@ -8,12 +8,14 @@ m      <- 5
 truth <- projFrictoinLin(t = t, y0 = y0, v0 = v0, theta0 = theta0, b = b, m = m)
 
 n <- 10
-df <- data.frame(y0=jit(y0,n), v0=jit(v0,n), theta0=jit(theta0,n),b=jit(b,n),m=jit(m,n))
+df <- data.frame(y0=jit(y0,n), v0=jit(v0,n), theta0=jit(theta0,n),b = jit(b,n),
+                 m=jit(m,n))
 
 MC <- list()
 
 for(i in 1:n){
-        MC[[length(List)+1]] <- with(df, projFrictoinLin(t = t, y0 = y0[i], 
-                                                         v0 = v0[i], theta0 = theta0[i], 
-                                                         b = b[i], m = m[i]))
+        MC[[i]] <- with(df, projFrictoinLin(t = t, y0 = y0[i], 
+                                                       v0 = v0[i], 
+                                                       theta0 = theta0[i], 
+                                                       b = b[i], m = m[i]))
 }
